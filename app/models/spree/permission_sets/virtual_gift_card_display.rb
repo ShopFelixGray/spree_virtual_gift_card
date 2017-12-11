8ea16aa34e19e33a4a7bdb68d7ca582c7e64ad94
hf2186 <hf2186@users.noreply.github.com>
2017-12-11 12:20:39 -0500

    Removing Solidus Code

--- app/models/spree/permission_sets/virtual_gift_card_display.rb
+++
@@ -1,9 +0,0 @@
-module Spree
-  module PermissionSets
-    class VirtualGiftCardDisplay < PermissionSets::Base
-      def activate!
-        can [:display, :admin], Spree::VirtualGiftCard
-      end
-    end
-  end
-end
