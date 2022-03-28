40b67732fb503247f78328667faf458a4135d45f
Mike <mikewalters@shopfelixgray.com>
2022-03-28 11:23:48 -0400

    Update to 3.7

--- config/initializers/line_item_controller_gift_card_details.rb
+++ config/initializers/line_item_controller_gift_card_details.rb
@@ -1,4 +1,4 @@
 Rails.application.config.to_prepare do
   Spree::Api::V1::LineItemsController.line_item_options += [gift_card_details: [:recipient_name, :recipient_email, :gift_message, :purchaser_name, :send_email_at]]
-  Spree::Order.line_item_comparison_hooks << 'gift_card_match'
+  Rails.application.config.spree.line_item_comparison_hooks << Spree::Order.gift_card_match
 end
